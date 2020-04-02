753b4d950ed706b64e7b826df382630168a2d327
Kate <ekaterina.granat@hys-enterprise.com>
2020-04-02 13:24:16 +0300

    First Typescript Module

--- build/index.js
+++ build/index.js
@@ -1,1 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('Hello World!');
+console.log('Hello 333!');
+var greeter_1 = require("./greeter");
+console.log(greeter_1.Greeter('Jonathan'));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 console.log('Hello World!');
+console.log('Hello 333!');
+
+import { Greeter } from "./greeter";
+console.log(Greeter('Jonathan'));
